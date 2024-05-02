6aec40119ff2f91e2b71be47b11e3aa006de07cb
Aldy <aldylbn@gmail.com>
2024-05-02 10:49:59 +0800

    'what'

--- account.js
+++ account.js
@@ -1,43 +1,49 @@
 const sqlite = require('sqlite3').verbose();
 
 
+function submitForm() {
+       var getData = {
+        username: document.getElementById("username").value,
+        email: document.getElementById("email").value,
+        password: document.getElementById("password").value
+    };
+}
+
 let sql;
 
 
+
+
+
 const db = new sqlite.Database('users.db', sqlite.OPEN_READWRITE, (err)=>{
-    if (err)return console.error(err.message)
+    if (err)return console.error(err)
 })
 
-    async function getData(url){
-    try{
 
 
-
-    }catch (error){console.log(err)
-
-
-
-    }
-
-
-
-    }
-    getData('users.db')
-    // sql = 'CREATE TABLE users(ID INTEGER PRIMARY KEY, Username, email, password)';
-    // db.run(sql);
     
-    // db.run("DROP TABLE users")
+     sql = 'INSERT INTO users(Username, email, password) VALUES (?,?,?)';
+    db.run(sql, [getData.username, getData.email, getData.password], (err) => {
+        if (err) return console.error(err)});
+        console.log("Data inserted successfully!");
 
-//USERNAME, EMAIL, PASSWORD 
-
-// sql= 'INSERT INTO users(Username, email, password) VALUES (?,?,?)';
-// db.run(sql,["fred", "fred@gmail.com", "geehhehet" ], (err)=>{
-//     if (err)return console.error(err.message)
-// })
-
-// sql= 'UPDATE users SET first_name = ? WHERE id = ?';
-// db.run(sql, ['jake', 1], (err)=>{
-//      if (err)return console.error(err.message) }  );
+    //      sql = 'INSERT INTO users(Username, email, password) VALUES (?,?,?)';
+    // db.run(sql, ["jake", "aladin@gmail.com", "password"], (err) => {
+    //     if (err) return console.error(err)});
+    //     console.log("Data inserted successfully!");
+        
+        
+    
+    //    sql = 'CREATE TABLE users(ID INTEGER PRIMARY KEY, Username, email, password)';
+    //    db.run(sql);
+       
+       // db.run("DROP TABLE users")
+   
+   //USERNAME, EMAIL, PASSWORD 
+    
+    // sql= 'UPDATE users SET first_name = ? WHERE id = ?';
+    // db.run(sql, ['jake', 1], (err)=>{
+        //      if (err)return console.error(err.message) }  );
 
 
 // sql = 'SELECT * FROM users'; db.all(sql, [], (err, rows)=>{if (err)return console.error(err.message)  ;
