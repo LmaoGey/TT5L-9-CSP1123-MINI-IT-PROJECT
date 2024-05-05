14647413c035a3e2cff92ce3c1710cf2f652938a
Yap Kah Jun <carsonyap0922@gmail.com>
2024-05-05 16:15:29 +0800

    edited the menubar

---
+++ profile.js
@@ -0,0 +1,9 @@
+//menu bar icon
+let menubar = document.querySelector('.menubar');
+let navigation = document.querySelector('.navigation');
+
+menubar.onclick = function(){
+    navigation. classList.toggle('active');
+};
+
+
