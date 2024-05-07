2174cb9bc5fc2729078ee8321cb6b51775751f6e
Yap Kah Jun <carsonyap0922@gmail.com>
2024-05-08 01:30:10 +0800

    edited content

--- profile.js
+++ profile.js
@@ -6,5 +6,21 @@ menubar.onclick = function(){
     navigation. classList.toggle('active');
 };
 
-////
+let section = document.querySelectorAll('section');
+let navlink = document.querySelectorAll('.selection a');
 
+window.onscroll = () => {
+    section.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navlink.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('.selection a [href*=' + id + ']').classList.add ('active');
+            });
+        };
+    });
+};
