3019a4a07db1a618881f645e8ca63ac25745bd64
kuansiing <chinkuansiing@gmail.com>
2024-05-08 11:54:33 +0800

    who

--- customise.js
+++ customise.js
@@ -14,19 +14,19 @@ containers.forEach(container => {
     container.addEventListener('dragover', e => {
         console.log('drag over');
         e.preventDefault();
-        const afterElement = getDragAfterElement(container, e.clientY);
+        const afterElement = getDragAfterElement(container, e.clientX);
         console.log(afterElement);
         const draggable = document.querySelector('.dragging'); 
         container.appendChild(draggable);
     });
 });
 
-function getDragAfterElement(container, y) {
+function getDragAfterElement(container, x) {
     const draggableElements = [...container.querySelectorAll('.draggable:not(.dragging)')]; 
 
     return draggableElements.reduce((closest, child) => {
         const box = child.getBoundingClientRect(); 
-        const offset = y - box.top - box.height / 2;
+        const offset = x - box.left - box.width / 2;
         console.log(offset);
         if (offset < 0 && offset > closest.offset) {
             return { offset: offset, element: child };
