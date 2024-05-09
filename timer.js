296dd08c20caf48424e334cb28820171028cafe8
Aldy <aldylbn@gmail.com>
2024-05-09 09:38:49 +0800

    yeaye

---
+++ timer.js
@@ -0,0 +1,27 @@
+const start = document.getElementById("startbutton") ;
+const pause = document.getElementById("pausebutton") ;
+const skip =  document.getElementById("skipbutton");
+const timething = document.getElementById("timer")
+
+
+function starttimer() {
+
+
+
+}
+
+
+function pausetimer(){
+
+
+}
+function skiptimer(){
+
+
+}
+
+
+
+start.addEventListener("click" , starttimer)
+pause.addEventListener("click" , pausetimer)
+skip.addEventListener("click" , skiptimer)
